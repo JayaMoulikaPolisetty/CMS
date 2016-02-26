5efd93e4ecaf30cf04b4da0179cb0569bbd113c0
mouli <mouli>
2016-02-26 15:24:44 +0530

    cms

---
+++ database/migrations/2016_02_23_101209_create_developer_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeveloperTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('developers', function (Blueprint $table) {
+            $table->text('header');
+             $table->increments('id');
+              $table->string('user');
+               $table->string('filename');
+               $table->timestamp('created_at');
+                $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
